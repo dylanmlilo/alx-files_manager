6271b2f56e2ab1d4915a74baddcf5d0dc008c89a
dylanmlilo <dylanmlilo12@gmail.com>
2024-10-01 23:33:46 -0700

    alx-files_manager

---
+++ utils/redis.js
@@ -0,0 +1,39 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
+  }
+
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
+    return value;
+  }
+
+  async set(key, value, time) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, value);
+    await this.client.expire(key, time);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
